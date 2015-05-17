fca9c702dc2f96126dfc0c43aa347eb9722ee5ff
Stephen Horvath <github@stephenhorvath.com>
2015-05-17 13:00:17 -0700

    Completing category selection, includes additive categories feature. [Finishes #93590102]

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,30 +1,16 @@
 
-var categoryData = {
-    rooftop_solar: {
-        id: 1,
-        exclusivity: [2]
-    },
-    offsite_solar: {
-        id: 2,
-        exclusivity: [1]
-    },
-    led: {
-        id: 3,
-        exclusivity: []
-    },
-    hybrid_car: {
-        id: 7,
-        exclusivity: [8, 9]
-    },
-    plug_in_hybrid: {
-        id: 8,
-        exclusivity: [7, 9]
-    },
-    electric_car: {
-        id: 9,
-        exclusivity: [7, 8]
-    }
+var categoryExclusions = {
+    rooftop_solar: [ 'offsite_solar' ],
+    offsite_solar: [ 'rooftop_solar' ],
+    electric_heat_pump: [ 'geothermal_heat_pump' ],
+    geothermal_heat_pump: [ 'electric_heat_pump'],
+    hybrid_car: [ 'plug_in_hybrid', 'electric_car' ],
+    plug_in_hybrid: [ 'hybrid_car', 'electric_car' ],
+    electric_car: [ 'hybrid_car', 'plug_in_hybrid' ]
 };
+
+// the keys of all of the currently selected categories
+var selectedCategories = [];
 
 // stores all of the data for the currently selected location
 var locationData;
@@ -33,7 +19,7 @@ var locationData;
 var engine = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.whitespace,
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    identify: function(obj) { return obj.id; },
+    identify: function( obj ) { return obj.id; },
     remote: {
         url: '/typeahead/%QUERY',
         wildcard: '%QUERY'
@@ -46,56 +32,89 @@ engine.initialize();
 
 function calculatorInit() {
     // instantiate the typeahead UI
-    $('#city_or_zip').typeahead({
+    $( '#city_or_zip' ).typeahead({
         hint: true,
         minLength: 2
     }, {
-        display: function(data) {
-            return formatCityState(data);
+        display: function( data ) {
+            return formatCityState( data );
         },
         source: engine.ttAdapter(),
         templates: {
             empty: 'No results found',
-            suggestion: function (data) {
-                return '<p>' + formatCityState(data) + '</p>';
+            suggestion: function ( data ) {
+                return '<p>' + formatCityState( data ) + '</p>';
             }
         }
-    }).bind('typeahead:select', function(event, suggestion) {
+    }).bind( 'typeahead:select', function( event, suggestion ) {
         $.ajax({
             url: '/calculate',
-            data: {id: suggestion.id},
+            data: { id: suggestion.id },
             method: 'post'
         })
-        .done(setLocationData);
+        .done( setLocationData );
     });
 
-    $.each(categoryData, function(key, value) {
-        $('.btn#' + key).click(key, categoryChange);
-    });
+    $( '.calculator .btn' ).click( categoryChange );
 }
 
-$(document).ready( calculatorInit ); // For direct page loads
-$(document).on( 'page:load', calculatorInit ); // For following links
+$( calculatorInit ); // For direct page loads
+$( document ).on( 'page:load', calculatorInit ); // For following links
 
-function formatCityState(cityState) {
-    return toTitleCase(cityState.city) + ', ' + cityState.state.toUpperCase();
+function formatCityState( cityState ) {
+    return toTitleCase( cityState.city ) + ', ' + cityState.state.toUpperCase();
 }
 
-function toTitleCase(str) {
-    return str.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+function toTitleCase( str ) {
+    return str.replace( /([^\W_]+[^\s-]*) */g, function( txt ) {
+        return txt.charAt( 0 ).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
 
-function setLocationData(data) {
+function setLocationData( data ) {
     //$('.calculator-result-total span#total').text("$1234");
-    $('.calculator-result-total span#city').text(formatCityState(data) + ".");
+    $( '.calculator-result-total span#city' )
+        .text( formatCityState( data ) + "." );
     locationData = data;
 }
 
-function categoryChange(obj) {
-    if( $('.btn#' + obj.data ).hasClass('active'))
-        $('.result-category#' + obj.data).removeClass('active');
+function categoryChange( obj ) {
+    if( this.classList.contains( 'active' ) )
+        removeCategory( this.id );
     else
-        $('.result-category#' + obj.data).addClass('active');
+        addCategory( this.id );
+
+    resetCategoryMessage();
+}
+
+function addCategory( name ) {
+    selectedCategories.push( name );
+
+    // remove any exclusive categories
+    if( name in categoryExclusions ) {
+        $.each(categoryExclusions[name], function( index, value ) {
+            $( '.btn#' + value).removeClass('active');
+            removeCategory( value );
+        });
+    };
+}
+
+function removeCategory( name ) {
+    var found = 0;
+    while( ( found = selectedCategories.indexOf( name, found ) ) !== -1 ) {
+        selectedCategories.splice( found, 1 );
+    }
+}
+
+function resetCategoryMessage() {
+    if( selectedCategories.length == 0 ) {
+        $( '.calculator-result' ).removeClass( 'active' );
+        return;
+    }
+
+    $( '.result-category' ).removeClass( 'active' );
+    var lastElement = selectedCategories[ selectedCategories.length-1 ];
+    var resultObj = $( '.result-category#' + lastElement );
+    resultObj.addClass('active');
+    $( '.calculator-result' ).addClass( 'active' );
 }
