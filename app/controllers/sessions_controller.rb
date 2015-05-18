14516ebd2254fa602ab0b4166f7cd8429bf497dc
Stephen Horvath <github@stephenhorvath.com>
2015-05-17 21:00:39 -0700

    Adding initial version of geo-ip lookup.  Eventually replace with a better service.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
 
+  def index
+    # TODO: Fix this hack.  Ip address should be retrieved/stored in session model.
+    ip = request.remote_ip
+    ip = '75.149.54.14' if ip == '127.0.0.1'
+    @geolocation = GeoIp.geolocation(ip)
+  end
+
   def calculate
     @location = SavingsLocation.find(params[:id])
-    # TODO: Store location in User model so if they register we'll have some data
+    # TODO: Store location in Session model so if they register we'll have some data
     # TODO: Write function to check if session already exists, and if not create a new one.
     #   Since multiple functions could potentially create a session object.
     # @session = Session.new(city/state/zip attribute hash from @savings)
