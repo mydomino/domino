1c214b92ff1f3626ae73ae6bc456a715199437cf
Stephen Horvath <github@stephenhorvath.com>
2015-05-27 22:40:51 -0700

    Fixed subdomain detection to work with staging server.

--- app/helpers/clones_helper.rb
+++ app/helpers/clones_helper.rb
@@ -1,6 +1,9 @@
 module ClonesHelper
   def current_clone
-    request.subdomain if Clone.all.map(&:name).include? request.subdomain
+    return if request.subdomain.empty?
+    match = /(\w+)/.match(request.subdomain)
+    clone = match.captures[0] if match
+    clone if Clone.all.map(&:name).include?(clone)
   end
 
 end
