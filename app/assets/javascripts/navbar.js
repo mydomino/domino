1e4818658ac6bdb97f67da5340a5a9a14aee6802
Josh Morrow <josh@mydomino.com>
2015-07-30 11:55:32 -0700

    Happy with burger/mobile nav state

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,9 +2,9 @@ $( window ).scroll( function( event ) {
     var scroll = $( window ).scrollTop();
     if( scroll < 300 ) {
         $( 'body.index .navbar, body.getstarted .navbar' )
-            .removeClass( 'scrolled animated slideInDown' );
+            .removeClass( 'scrolled animated slideInDown fixed' );
     } else {
         $( 'body.index .navbar, body.getstarted .navbar' )
-            .addClass( 'scrolled animated slideInDown' );
+            .addClass( 'scrolled animated slideInDown fixed' );
     }
 });
