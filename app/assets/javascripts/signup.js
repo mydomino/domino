2e8181d1bd286b45942b5179ce224d9edce44a37
Josh Morrow <josh@mydomino.com>
2015-08-05 15:53:45 -0700

    Adding ooptimizely event tracking to signup tracking

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -3,6 +3,8 @@ function track_lead_creation()
   ga('send', 'event', 'lead', 'created');
   _fbq.push(['track','6031072723663',{}]);
   heap.track('lead creation', {});
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(["trackEvent", "leadCreated"]);
 }
 function animate_form_success()
 {
@@ -10,5 +12,4 @@ function animate_form_success()
   form.addClass('animated fadeOutDown');
   success = $('.success');
   success.show().addClass('animated fadeInDown');
-
 }
