fbc5f5c51f1c4b415ff5ad316daa52f0211045de
Josh Morrow <josh@mydomino.com>
2015-08-05 12:49:17 -0700

    Small changes to form errors

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 require 'pages_controller'
 
 describe PagesController do
-  it 'displays the home page'
+  it 'displays the home page' do
+    get :index
+
+    expect(response).to render('index')
+  end
   it 'displays the about page'
   it 'displays the terms page'
 end
