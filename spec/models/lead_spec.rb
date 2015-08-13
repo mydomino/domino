5ffcad285cb46fa20f9b2189ad7b418b3e4e079b
Josh Morrow <josh@mydomino.com>
2015-08-13 14:01:23 -0700

    All tests pass but I just realized I'm a dodo and should be using the SMTP API

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'helpers/zoho_mock'
+require 'helpers/mandrill_mock'
 
 describe Lead, type: :model do
   it 'has a valid factory' do
@@ -45,7 +46,35 @@ describe Lead, type: :model do
     lead = FactoryGirl.create(:lead)
 
     expect(zoho_lead).to have_received(:save)
-
   end
+
+  it 'send an email to the correct address' do
+    mandrill = spy('mandrill')
+    #mandrill send function is monkeypatched
+    allow(mandrill.messages).to receive(:send)
+    allow(Mandrill::API).to receive(:new).and_return(mandrill)
+    email = Faker::Internet.email
+    
+    lead = FactoryGirl.create(:lead, email: email)
+
+    expect(mandrill.messages).to have_received(:send).with({"headers"=>{"Reply-To"=>"myconcierge@mydomino.com"},
+     "track_clicks"=>true,
+     "track_opens"=>true,
+     "from_email"=>"amy@mydomino.com",
+     "from_name"=>"Amy Gormin",
+     "text"=>"Thank you for contacting Domino! Our fabulous energy savings concierge team will contact you soon!",
+     "inline_css"=>nil,
+     "track_opens"=>nil,
+     "to"=>[{"email"=>email}],
+     "html"=>'<p>Thank you for contacting Domino!</p><p>Our fabulous energy savings concierge team will contact you soon!</p><p>Warmly, The Domino Team</p>',
+     "important"=>false,
+     "auto_text"=>true,
+     "subject"=>"Thanks from Domino",
+     "merge"=>true,
+     "signing_domain"=>"mydomino.com",
+     "view_content_link"=>nil,
+     "preserve_recipients"=>true})
+  end
+
   it 'geocodes its ip upon creation'
 end
