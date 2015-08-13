4905497f0573a9055f1c7e185edd794cbdbce1bf
Josh Morrow <josh@mydomino.com>
2015-08-13 16:05:12 -0700

    Adding Amy's name to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
   def welcome_email(email)
-    mail(from: 'amy@mydomino.com', to: email, subject: 'Thanks From Domino')
+    mail(from: '"Amy Gorman" amy@mydomino.com', to: email, subject: 'Thanks From Domino')
   end
 end
