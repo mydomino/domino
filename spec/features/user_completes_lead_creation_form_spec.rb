a872c5aff727170c28925fb4becb19e41361b593
Josh Morrow <josh@mydomino.com>
2015-08-17 10:59:42 -0700

    making name field visible

--- spec/features/user_completes_lead_creation_form_spec.rb
+++ spec/features/user_completes_lead_creation_form_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 require 'rack_session_access/capybara'
 require 'helpers/zoho_mock'
+require 'faker'
 WebMock.disable_net_connect!(:allow => "127.0.0.1")
 
 RSpec.feature "User completes lead creation form", :type => :feature, :js => true do
   scenario "with valid data and expects form to be disabled" do
     visit getstarted_path
 
-    fill_in "lead_email", with: "josh@mydomino.com"
-    fill_in "lead_phone", with: "6078575974"
+    fill_in "lead_first_name", with: Faker::Name.first_name
+    #fill_in "lead_last_name", with: Faker::Name.last_name
+    fill_in "lead_email", with: Faker::Internet.email
+    fill_in "lead_phone", with: Faker::PhoneNumber.phone_number
     find('form input[type="submit"]').click 
 
     expect(page).to have_css('form', visible: false)
     expect(page).not_to have_css('.errors li')
     expect(page).to have_css('.success', visible: true)
   end
-  scenario "with invalid data" do
+  scenario "without any data" do
 
     visit getstarted_path
     find('form input[type="submit"]').click 
 
     expect(page).to have_css('.errors li', text: "Email can't be blank")
   end
+
 end
 RSpec.feature "utm_campaign is properly captured", :type => :feature do
   scenario "with a utm_campaign set" do
