dc55c319ce43fec0a4c74e9f429179552e7d4ec4
Josh Morrow <josh@mydomino.com>
2015-08-28 11:01:36 -0700

    Scraping a random product url via faker and google

--- spec/features/concierge_creates_new_amazon_product_spec.rb
+++ spec/features/concierge_creates_new_amazon_product_spec.rb
@@ -13,4 +13,22 @@ RSpec.feature "Concierge creates a new product", :type => :feature do
 
     expect(page).to have_content(AmazonProduct.first.name) #should be the Nest Name
   end
+
+  scenario "by inputting the item's url", focus: true, js: true do 
+    product_url = random_amazon_url
+    visit new_amazon_product_path
+    fill_in "Product URL", with: product_url
+    click_on "Create"
+  end
+end
+
+def random_amazon_url
+  query = "Amazon #{Faker::Commerce.product_name}"
+  google query
+end
+
+def google term
+  visit "http://google.com/search?q=#{URI::encode(term)}"
+  first('#ires h3 a').click
+  return page.current_url
 end
