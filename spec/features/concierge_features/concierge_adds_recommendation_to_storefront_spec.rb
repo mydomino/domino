695891e92efe06a401d0e4a11097b9b235a8b226
Josh Morrow <josh@mydomino.com>
2015-09-03 12:12:15 -0700

    Failing test for rec creation

--- spec/features/concierge_features/concierge_adds_recommendation_to_storefront_spec.rb
+++ spec/features/concierge_features/concierge_adds_recommendation_to_storefront_spec.rb
@@ -9,7 +9,9 @@ RSpec.feature "Concierge makes a recommendation", focus: true do
     login_as(concierge, scope: :concierge)
 
     visit amazon_storefronts_path
-    
     click_on "Make Recommendation"
+    select storefront.lead_name
+    fill_in 'comment', with: Faker::Lorem.paragraph
+    click_on 'Save'
   end
 end
