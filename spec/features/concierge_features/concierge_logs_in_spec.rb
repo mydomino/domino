b704c478c54e6a13c4084d5816bed57953e9dc15
Josh Morrow <josh@mydomino.com>
2015-09-14 13:17:35 -0700

    Tasks are considerably more responsive

--- spec/features/concierge_features/concierge_logs_in_spec.rb
+++ spec/features/concierge_features/concierge_logs_in_spec.rb
@@ -9,8 +9,7 @@ RSpec.feature "Concierge logs in", :type => :feature do
     visit new_concierge_session_path
     fill_in "Email", with: concierge.email
     fill_in "Password", with: concierge.password
-    click_on "Log in"
-
+    click_on "Enter"
     
     expect(current_path).to eq(amazon_storefronts_path)
   end
