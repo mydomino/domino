c6b5f3901437514a0cfbca6f87a335ee6a7bd5bc
Josh Morrow <josh@mydomino.com>
2015-09-15 15:04:11 -0700

    Removing leaf from icon options on task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  ICON_OPTIONS = ['energy', 'heat', 'vehicle', 'leaf']
+  ICON_OPTIONS = ['energy', 'heat', 'vehicle']
   validates_inclusion_of :icon, :in => ICON_OPTIONS
 
 
