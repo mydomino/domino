42dc7bc1fb6fbc1249911d7cca3bc1c581aafd99
Josh Morrow <josh@mydomino.com>
2015-09-15 13:40:23 -0700

    Fixing comment structure

--- app/assets/javascripts/storefront.js
+++ app/assets/javascripts/storefront.js
@@ -4,7 +4,6 @@ function(){
     var target = $(event.target)
     target.hide();
     var reveal_class = target.data('reveal-next')
-    console.log(reveal_class)
     target.next('.' + reveal_class).show();
   })
 });
