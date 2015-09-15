7f8d7164a22b48494fdc3d4d184d7a7467c3e5eb
Josh Morrow <josh@mydomino.com>
2015-09-15 13:53:19 -0700

    Making edit button replace text with comment

--- app/assets/javascripts/storefront.js
+++ app/assets/javascripts/storefront.js
@@ -2,6 +2,9 @@ $(document).ready(
 function(){
   $('*[data-reveal-next]').click(function(event){
     var target = $(event.target)
+    target.parent().contents().filter(function(){
+        return (this.nodeType == 3);
+    }).remove();
     target.hide();
     var reveal_class = target.data('reveal-next')
     target.next('.' + reveal_class).show();
