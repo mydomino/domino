f9cbe9abe4dea55d7586780e7c3936f166e7c572
Josh Morrow <josh@mydomino.com>
2015-09-17 15:47:27 -0700

    Making default done state false

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -4,14 +4,18 @@ class Recommendation < ActiveRecord::Base
   belongs_to :concierge
   validates :recommendable_id, uniqueness: {scope: [:dashboard_id, :recommendable_type] }
   validates :dashboard_id, presence: true
-  default_scope { order("done DESC") }
+  default_scope { order("done ASC") }
   scope :tasks, -> { where(recommendable_type: 'Task') }
   scope :products, -> { where(recommendable_type: 'Product') }
-  after_create :assign_concierge
+  after_create :assign_concierge, :set_done_to_false
   scope :done, -> { where(done: true) }
 
   def assign_concierge
     self.update_attribute(:concierge_id, dashboard.concierge_id)
+  end
+
+  def set_done_to_false
+    self.update_attribute(:done, false)
   end
 
   def global_recommendable
@@ -25,4 +29,5 @@ class Recommendation < ActiveRecord::Base
   def global_recommendable=(recommendable_gid)
     self.recommendable = GlobalID::Locator.locate recommendable_gid
   end
+
 end
