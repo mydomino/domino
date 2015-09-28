052a94a9ec88e21f57dc9a2996ebf1ae9dc37cde
Josh Morrow <josh@mydomino.com>
2015-09-28 14:20:17 -0700

    Fixing test by being more capybara-ish

--- spec/features/concierge_features/concierge_deletes_dashboard_spec.rb
+++ spec/features/concierge_features/concierge_deletes_dashboard_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Concierge deletes dashboard" do
   let(:concierge) { FactoryGirl.create(:concierge) }
   let!(:dashboard) {FactoryGirl.create(:dashboard, concierge: concierge)}
 
-  scenario "by hitting the delete button", focus:true, js: true do
+  scenario "by hitting the delete button", js: true do
     login_as(concierge, scope: :concierge)
 
     visit dashboards_path
@@ -13,7 +13,7 @@ RSpec.feature "Concierge deletes dashboard" do
     expect(page).to have_content dashboard.lead_name
     click_on "Delete"
     page.driver.browser.switch_to.alert.accept
-
+    expect(page).not_to have_content(dashboard.lead_name)
     expect(Dashboard.count).to eq(0)
   end
 end
