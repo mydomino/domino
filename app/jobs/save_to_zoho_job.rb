6832b74eea42f23d5d228641bc86eb5ae3e03ed9
Josh Morrow <josh@mydomino.com>
2015-10-28 10:47:58 -0700

    Fixed I think

--- app/jobs/save_to_zoho_job.rb
+++ app/jobs/save_to_zoho_job.rb
@@ -2,28 +2,26 @@ class SaveToZohoJob < ActiveJob::Base
   queue_as :default
 
   def perform(lead)
-    zoho_lead = RubyZoho::Crm::Lead.new(
-      first_name: lead.first_name,
-      last_name: lead.last_name,
-      email: lead.email,
-      phone: lead.phone,
-      street: lead.address,
-      city: lead.city,
-      state: lead.state,
-      source: lead.source,
-      ip_address: lead.ip,
-      referrer: lead.referer,
-      #can just be start_time - created_at
-      time_on_site: lead.created_at - lead.start_time,
-      campaign: lead.campaign,
-      browser: lead.browser
-      )
-    byebug
-    if(lead.get_started.present?)
-      zoho_lead.interested_in_solar = lead.get_started.solar
-      zoho_lead.interested_in_energy_plan = lead.get_started.energy_analysis
-      zoho_lead.average_electric_bill = lead.get_started.average_electric_bill
-      zoho_lead.zip_code = lead.get_started.area_code
+    zoho_lead = RubyZoho::Crm::Lead.new.tap do |zoho_lead|
+      zoho_lead.first_name = lead.first_name
+      zoho_lead.last_name = lead.last_name
+      zoho_lead.email = lead.email
+      zoho_lead.phone = lead.phone
+      zoho_lead.street = lead.address
+      zoho_lead.city = lead.city
+      zoho_lead.state = lead.state
+      zoho_lead.source = lead.source
+      zoho_lead.ip_address = lead.ip
+      zoho_lead.referrer = lead.referer
+      zoho_lead.time_on_site = lead.created_at - lead.start_time
+      zoho_lead.campaign = lead.campaign
+      zoho_lead.browser = lead.browser
+      if(lead.get_started.present?)
+        zoho_lead.interested_in_solar = lead.get_started.solar
+        zoho_lead.interested_in_energy_plan = lead.get_started.energy_analysis
+        zoho_lead.average_electric_bill = lead.get_started.average_electric_bill
+        zoho_lead.zip_code = lead.get_started.area_code
+      end
     end
     zoho_lead.save
     lead.update_attributes(saved_to_zoho: true)
