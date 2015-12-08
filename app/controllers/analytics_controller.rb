8f70d4a871acd3b0bfdfe640838aae6cc3cc3cba
Josh Morrow <josh@mydomino.com>
2015-12-08 14:28:43 -0800

    Adding filters to activity

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,6 +3,13 @@ class AnalyticsController < ApplicationController
   layout 'concierge'
 
   def show
+    @filter = params[:filter]
+    if(@filter == 'all')
+      @recent_events = Recommendation.done.timestamped.last(10)
+    else
+      @filter = 'mine'
+      @recent_events = Recommendation.done.timestamped.where(concierge: current_concierge).last(10)
+    end
     @dashboard_count = Dashboard.all.count
     @recommendations_count = Recommendation.all.count
     @recommendations_completed_count = Recommendation.done.count
