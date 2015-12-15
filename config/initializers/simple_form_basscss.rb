5414d3394d950341b88b8d48ca20cec7d6cfff96
Josh Morrow <josh@mydomino.com>
2015-12-14 16:14:59 -0800

    Sign in screen basscssed

--- config/initializers/simple_form_basscss.rb
+++ config/initializers/simple_form_basscss.rb
@@ -21,7 +21,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.wrapper :tag => 'div' do |ba|
       ba.use :label, class: 'block', :wrap_with => { :tag => 'div', :class => 'clearfix' }
-      ba.use :input
+      ba.use :input, class: 'field block'
       ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
       ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
     end
