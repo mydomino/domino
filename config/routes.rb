221790787b18553324d6c84573735c2f38f4b948
Josh Morrow <josh@mydomino.com>
2016-01-06 11:58:21 -0800

    Adjusting routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'privacy' => 'pages#privacy'
   #get 'solar' => 'pages#solar'
 
+  #concierge routes
   devise_for :concierges, skip: [:registrations]                                   
   as :concierge do
     get 'concierges/edit' => 'registrations#edit', :as => 'edit_concierge_registration'    
@@ -15,7 +16,9 @@ Rails.application.routes.draw do
   end
   resources :concierges, only: [:update]
 
+  #lead routes
   resources :leads, only: [:create, :new, :index]
+
   resources :dashboards, path: '/dashboard/', only: [:new, :create, :show, :index, :destroy] do
     resources :recommendations, only: [:new, :create]
     patch 'bulk_update' => 'recommendations#bulk_update', as: 'bulk_update'
@@ -26,19 +29,14 @@ Rails.application.routes.draw do
     delete 'undo' => 'recommendations#undo', as: 'undo_complete'
   end
 
-  resource :analytics, only: [:show]
-
   resources :products do
     post 'toggle_default' => 'products#toggle_default'
   end
-
   post 'products/update-prices' => 'products#update_all_amazon_prices', as: 'update_product_prices'
   
   resources :tasks do
     post 'toggle_default' => 'tasks#toggle_default'
   end
-
-  resources :contests
 
   get 'getstarted' => 'leads#new'
   resource :get_started, only: [:show, :create] do
@@ -48,10 +46,13 @@ Rails.application.routes.draw do
     post 'finish' => 'get_starteds#finish', as: 'finish'
   end
 
+  resources :contests
   get '/smart-home' => 'leads#energy_awareness'
 
-  get '/blog' => redirect("/blog/")
+  resource :analytics, only: [:show]
 
   match "/delayed_job" => DelayedJobWeb, :anchor => false, via: [:get, :post]
+
+  get '/blog' => redirect("/blog/")
 
 end
