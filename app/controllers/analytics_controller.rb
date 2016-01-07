b97e7967b640d4c54dee13ea054ab1ea26a40134
Josh Morrow <josh@mydomino.com>
2016-01-06 16:03:26 -0800

    Descending states

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -14,7 +14,7 @@ class AnalyticsController < ApplicationController
     @recommendations_count = Recommendation.all.count
     @recommendations_completed_count = Recommendation.done.count
     @recommendations = Recommendation.all
-    @dashboards_by_state = Dashboard.joins('join leads on leads.email = dashboards.lead_email').where('leads.state IS NOT NULL').group("leads.state").count
+    @dashboards_by_state = Dashboard.joins('join leads on leads.email = dashboards.lead_email').where('leads.state IS NOT NULL').group("leads.state").order("COUNT(leads.state) DESC").count
   end
 
 end
