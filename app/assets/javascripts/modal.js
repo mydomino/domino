c6ce1d8a51a20c4e9c656a41d1a125283d072b9d
Josh Morrow <josh@mydomino.com>
2016-01-12 12:10:57 -0800

    adding fade-in for modals

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,23 +1,31 @@
 $(document).ready(function()
 {
   $('[data-toggle="modal"]').click(function(e){
-   var target = $(e.currentTarget).data('target');
-   $(target).show();
-   $('body').append('<div class="modal-bg"></div>');
-   $('body').addClass('overflow-hidden');
+    var target = $(e.currentTarget).data('target');
+    reveal_modal(target);
   });
   $('.modal').click(function(e)
   {
     if($(e.originalEvent.target).parents('.modal-content').length < 1)
     {
-      $('.modal-bg').remove();
-      $('.modal').hide();
-      $('body').removeClass('overflow-hidden');
+      hide_modal(); 
     }
   });
   $('[data-dismiss=modal]').click(function(){
-      $('.modal-bg').remove();
-      $('.modal').hide();
-      $('body').removeClass('overflow-hidden');
+    hide_modal();
   })
 });
+
+function reveal_modal(target)
+{
+  $(target).show().addClass("animated fadeIn");
+  $('body').append('<div class="modal-bg"></div>');
+  $('body').addClass('overflow-hidden');
+}
+
+function hide_modal()
+{
+  $('.modal-bg').remove();
+  $('.modal').hide();
+  $('body').removeClass('overflow-hidden');
+}
