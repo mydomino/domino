19e465eb78f1f53c8f4d30045880d6a63b3c85f8
Josh Morrow <josh@mydomino.com>
2016-01-13 11:40:13 -0800

    Fixing failing specs

--- spec/features/concierge_features/concierge_creates_new_amazon_product_spec.rb
+++ spec/features/concierge_features/concierge_creates_new_amazon_product_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature "Concierge creates a new product", :type => :feature do
     fill_in "Product URL", with: 'http://www.amazon.com/gp/product/B00MMLTUG0/ref=s9_simh_gw_p422_d0_i3?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-3&pf_rd_r=1M5H0WVH8YS1CKTXPRMA&pf_rd_t=36701&pf_rd_p=2084660942&pf_rd_i=desktop'
     click_on "Add Product"
     fill_in "Name", with: "New Name"
-    click_on "Save"
+    click_on "Update Product"
 
     expect(current_path).to eq(products_path)
     expect(page).to have_content("New Name")
