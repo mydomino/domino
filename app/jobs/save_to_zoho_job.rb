51f297c4104c84c27ad6df2ad11e1a68a0f2012c
marciandmnd <marciandmnd@yahoo.com>
2016-03-03 16:19:17 -0800

    fixed syntax error assigning lead email during zoho job

--- app/jobs/save_to_zoho_job.rb
+++ app/jobs/save_to_zoho_job.rb
@@ -5,7 +5,7 @@ class SaveToZohoJob < ActiveJob::Base
     zoho_lead = RubyZoho::Crm::Lead.new.tap do |zoho_lead|
       zoho_lead.first_name = lead.first_name
       #last_name is a required field in zoho
-      zoho_lead.last_name = lead.last_name.empty? ? "Not given" || lead.last_name 
+      zoho_lead.last_name = lead.last_name.empty? ? "Not given" : lead.last_name 
       zoho_lead.email = lead.email
       zoho_lead.phone = lead.phone
       zoho_lead.street = lead.address
