b55974825f5c7414f2597141aac35b87bc70541e
marciandmnd <marciandmnd@yahoo.com>
2016-03-09 10:52:41 -0800

    removed n+1 queries from analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -5,10 +5,10 @@ class AnalyticsController < ApplicationController
   def show
     @filter = params[:filter]
     if(@filter == 'all')
-      @recent_events = Recommendation.done.includes(:recommendable, :concierge).timestamped.order(:updated_at).last(50)
+      @recent_events = Recommendation.done.includes(:recommendable, :dashboard).timestamped.order(:updated_at).last(50)
     else
       @filter = 'mine'
-      @recent_events = Recommendation.done.includes(:recommendable, :concierge).timestamped.where(concierge_id: current_concierge.id).order(:updated_at).last(50)
+      @recent_events = Recommendation.done.includes(:recommendable, :dashboard).timestamped.where(concierge_id: current_concierge.id).order(:updated_at).last(50)
     end
     @dashboard_count = Dashboard.all.count
     @recommendations_count = Recommendation.all.count
@@ -16,5 +16,4 @@ class AnalyticsController < ApplicationController
     @recommendations = Recommendation.all
     @dashboards_by_state = Dashboard.joins('join leads on leads.email = dashboards.lead_email').where('leads.state IS NOT NULL').group("leads.state").order("COUNT(leads.state) DESC").count
   end
-
 end
