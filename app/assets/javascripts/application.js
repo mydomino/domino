49d51ebf7023e88ef77100bfe5373d7b87f09e3d
marciandmnd <marciandmnd@yahoo.com>
2016-03-23 19:10:36 -0700

    removed csrf from onboarding modules; instead setup ajax to use csrf token from head meta tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,12 @@
 //= require formance
 //= require storefront
 //= require modal
+
+$(function(){
+  $.ajaxSetup({
+    beforeSend: function( xhr ) {
+      var token = $('meta[name="csrf-token"]').attr('content');
+      if (token) xhr.setRequestHeader('X-CSRF-Token', token);
+    }
+  });
+});
