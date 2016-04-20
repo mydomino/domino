3c0f4972a34682861ee39bb8e67273114550b603
marciandmnd <marciandmnd@yahoo.com>
2016-04-20 10:08:52 -0700

    creation of new welcome email template

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,8 @@ class UserPreview < ActionMailer::Preview
     email = "foo@bar.com"
 		UserMailer.welcome_email(email)
 	end
+
+  def welcome_test
+    UserMailer.welcome_email_test("foo@bar.com")
+  end
 end
