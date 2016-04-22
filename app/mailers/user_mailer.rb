61f9dcb4f7fa5b25ffe3187ae80027633a6ce7f5
marciandmnd <marciandmnd@yahoo.com>
2016-04-21 17:36:46 -0700

    created legacy user registration email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ActionMailer::Base
   def welcome_email(email)
     @profile = Profile.find_by_email(email)
-    mail(from: '"Amy Gorman" <amy@mydomino.com>', to: email, subject: 'Thanks From Domino')
-    @profile.destroy
+    mail(from: '"Amy Gorman" <amy@mydomino.com>', to: email, subject: 'Welcome to MyDomino!')
   end
 
-  def registration_email(email)
-    @email = email
-    mail(from: '"Amy Gorman" <amy@mydomino.com>', to: @email, subject: 'Get your Action Dashboard')
+  def legacy_user_registration_email(email)
+    @profile = Profile.find_by_email(email)
+    mail(from: '"Amy Gorman" <amy@mydomino.com>', to: @email, subject: 'Announcing a new look and login for MyDomino!')
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,4 +4,9 @@ class UserPreview < ActionMailer::Preview
 		UserMailer.welcome_email(@profile.email)
 	end
 
+  def legacy_user_registration
+    @profile = Profile.create(first_name: 'Foo', last_name: 'Bar', email: "foobar@mydomino.com")
+    UserMailer.legacy_user_registration_email(@profile.email)
+  end
+
 end
