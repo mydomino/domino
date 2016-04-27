8ff6afaa0ec88aa6044653833661fdaad3c861c3
Jimmy <jyng02@gmail.com>
2016-04-27 16:35:26 -0700

    typography email template

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 20160427203624) do
     t.integer  "profile_id"
   end
 
-  create_table "clones", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "concierges", force: :cascade do |t|
     t.string   "name"
     t.string   "picture"
@@ -78,7 +72,6 @@ ActiveRecord::Schema.define(version: 20160427203624) do
     t.integer  "user_id"
   end
 
-  add_index "dashboards", ["concierge_id"], name: "index_dashboards_on_concierge_id", using: :btree
   add_index "dashboards", ["user_id"], name: "index_dashboards_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
@@ -96,6 +89,14 @@ ActiveRecord::Schema.define(version: 20160427203624) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "domino_products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price_cents"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "get_starteds", force: :cascade do |t|
     t.boolean  "solar"
@@ -201,6 +202,7 @@ ActiveRecord::Schema.define(version: 20160427203624) do
     t.string   "zip_code"
     t.string   "housing"
     t.integer  "avg_electrical_bill",  default: 0
+    t.integer  "availability_id"
     t.text     "comments"
     t.string   "partner_code"
     t.boolean  "onboard_complete",     default: false
@@ -214,6 +216,7 @@ ActiveRecord::Schema.define(version: 20160427203624) do
     t.string   "browser"
   end
 
+  add_index "profiles", ["availability_id"], name: "index_profiles_on_availability_id", using: :btree
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "recommendations", force: :cascade do |t|
@@ -227,9 +230,6 @@ ActiveRecord::Schema.define(version: 20160427203624) do
     t.datetime "updated_at"
     t.integer  "updated_by"
   end
-
-  add_index "recommendations", ["dashboard_id"], name: "index_recommendations_on_dashboard_id", using: :btree
-  add_index "recommendations", ["recommendable_id", "recommendable_type"], name: "recommendable_index", using: :btree
 
   create_table "tasks", force: :cascade do |t|
     t.string   "icon"
@@ -267,5 +267,6 @@ ActiveRecord::Schema.define(version: 20160427203624) do
   add_foreign_key "dashboards", "users"
   add_foreign_key "interests", "offerings"
   add_foreign_key "interests", "profiles"
+  add_foreign_key "profiles", "availabilities"
   add_foreign_key "profiles", "users"
 end
