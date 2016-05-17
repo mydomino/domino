5b605365b161ccf459c1fb2c24e1ef86e18e5d9a
marciandmnd <marciandmnd@yahoo.com>
2016-05-17 16:40:24 -0700

    universal logo js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,18 @@
 var navbarScroll = function(startColor, scrollColor){
+  //initial state
+  $('#logo-text').css('color', logoColors.startColors.text);
+  $('#logo-penguin').css('color', logoColors.startColors.penguin);
   $('.navbar-element').css('color', startColor);
   $(window).scroll(function() {
       var scrollTop = $(this).scrollTop();
       if ( scrollTop > 70) { 
+        $('#logo-text').css('color', logoColors.scrollColors.text);
+        $('#logo-penguin').css('color', logoColors.scrollColors.penguin);
         $('#navbar').css({'background-color':'#FFFFFF', 'box-shadow': '0 1px 1px 1px #f1f1f1'});
         $('.navbar-element').css('color', scrollColor)
       }else{
+        $('#logo-text').css('color', logoColors.startColors.text);
+        $('#logo-penguin').css('color', logoColors.startColors.penguin);
         $('#navbar').css({'background-color':'transparent', 'box-shadow' : 'none'});
         $('.navbar-element').css('color', startColor)
       }
