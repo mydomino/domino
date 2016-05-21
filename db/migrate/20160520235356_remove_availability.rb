e5e15083556ddcea64816e7d280040b4c7fdd277
marciandmnd <marciandmnd@yahoo.com>
2016-05-20 18:36:46 -0700

    fixed typo in remove profile fk migration

--- db/migrate/20160520235356_remove_availability.rb
+++ db/migrate/20160520235356_remove_availability.rb
@@ -1,6 +1,6 @@
 class RemoveAvailability < ActiveRecord::Migration
   def change
-    remove_foreign_key :availabilities, :profile_id
+    remove_foreign_key :availabilities, :profile
     drop_table :availabilities
   end
 end
