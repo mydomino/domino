9f7603752e7aeae809bc37172769dd60a6879a59
marciandmnd <marciandmnd@yahoo.com>
2016-06-21 17:35:20 -0700

    started adding new button components to web site

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -13,21 +13,23 @@ var navbarScroll = function(startColor, scrollColor){
   $('#logo-text > *').css('fill',logoColors.startColors.text)
   $('#logo-penguin > *').css('fill', logoColors.startColors.penguin);
   $('.navbar-element').css('color', startColor);
-  // $('#link-login').css({'borderColor':startColor});
   $(window).scroll(function() {
       var scrollTop = $(this).scrollTop();
       if ( scrollTop > 35) {
         $('#logo-text > *').css('fill',logoColors.scrollColors.text)
         $('#logo-penguin > *').css('fill', logoColors.scrollColors.penguin);
         $('#navbar').css({'background-color':'#FFFFFF', 'box-shadow': '0 1px 1px 1px #f1f1f1'});
-        // $('.navbar-element').css('color', scrollColor);
-        $('#link-login').css({'borderColor':scrollColor});
+        $('.navbar-element').css('color', scrollColor);
+        $('#link-login').addClass('btn-secondary');
+        $('#link-login').removeClass('btn-white');
+
       }else{
         $('#logo-text > *').css('fill', logoColors.startColors.text);
         $('#logo-penguin > *').css('fill', logoColors.startColors.penguin);
         $('#navbar').css({'background-color':'transparent', 'box-shadow' : 'none'});
         $('.navbar-element').css('color', startColor);
-        $('#link-login').css({'borderColor':startColor});
+        $('#link-login').addClass('btn-white');
+        $('#link-login').removeClass('btn-secondary');
       }
     });
 }
