49df390eff101c53735e6583c81d458878e93787
marciandmnd <marciandmnd@yahoo.com>
2016-06-24 13:06:09 -0700

    updated lead import script

--- lib/import_leads.rb
+++ lib/import_leads.rb
@@ -1,22 +1,22 @@
 require 'csv'
 
-Profile.skip_callback(:create, :after, :save_to_zoho)
+# Profile.skip_callback(:create, :after, :save_to_zoho)
 path = "#{File.expand_path(File.dirname(__FILE__))}/JoshFox_ZohoImport_06232016.csv"
-puts path
+# puts path
 leads = CSV.read(path, headers:true)
 leads.each do |row|
-  lead = Profile.create(
-                            first_name: row["First Name"],
-                            last_name: row["Last Name"],
-                            email: row["Email"],
-                            address_line_1: row["Street"],
-                            city: row["City"],
-                            state: row["State"],
-                            zip_code: row["Zip Code"],
-                            phone: row["Phone"],
-                            partner_code_id: PartnerCode.find_by_partner_name("Josh Fox").id,
-                            onboard_complete: true
-                          )
+#   lead = Profile.create(
+#                             first_name: row["First Name"],
+#                             last_name: row["Last Name"],
+#                             email: row["Email"],
+#                             address_line_1: row["Street"],
+#                             city: row["City"],
+#                             state: row["State"],
+#                             zip_code: row["Zip Code"],
+#                             phone: row["Phone"],
+#                             partner_code_id: PartnerCode.find_by_partner_name("Josh Fox").id,
+#                             onboard_complete: true
+#                           )
   case row["Concierge Lead Owner"]
   when "Laura Osburn"
     @concierge = 'laura@mydomino.com'
@@ -34,19 +34,21 @@ leads.each do |row|
         "&scope=crmapi"\
         "&xmlData=<Leads><row no='1'>"\
         "<FL val='Lead Owner'>#{@concierge}</FL>"\
-        "<FL val='First Name'>#{lead.first_name}</FL>"\
-        "<FL val='Last Name'>#{lead.last_name}</FL>"\
-        "<FL val='Email'>#{lead.email}</FL>"\
-        "<FL val='Lead Source'>CatchingTheSun</FL>"\
-        "<FL val='City'>#{lead.city}</FL>"\
-        "<FL val='State'>#{lead.state}</FL>"\
-        "<FL val='Zip Code'>#{lead.zip_code}</FL>"\
-        "<FL val='Partner Code'>#{lead.partner_code.code if lead.partner_code}</FL>"\
-        "<FL val='Partner Code Name'>#{lead.partner_code.partner_name if lead.partner_code }</FL>"\
+        "<FL val='First Name'>#{row['First Name']}</FL>"\
+        "<FL val='Last Name'>#{row['Last Name']}</FL>"\
+        "<FL val='Email'>#{row['Email']}</FL>"\
+        "FL val='Phone'>#{row['Phone']}</FL>"\
+        "<FL val='Lead Source'>Josh Fox</FL>"\
+        "<FL val='Street'>#{row['street']}</FL>"\
+        "<FL val='City'>#{row['City']}</FL>"\
+        "<FL val='State'>#{row['State']}</FL>"\
+        "<FL val='Zip Code'>#{row['Zip Code']}</FL>"\
+        "<FL val='Partner Code'>JOSHFOX</FL>"\
+        "<FL val='Partner Code Name'>Josh Fox</FL>"\
         "<FL val='Dashboard Been Registered?'>No</FL>"\
-        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{lead.email}</FL>"\
+        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{row['Email']}</FL>"\
         "<FL val='Onboard Complete'>Yes</FL>"\
-        "<FL val='Description'>Auto Onboard June 23, 2016</FL>"\
+        "<FL val='Description'>Auto Onboard June 24, 2016</FL>"\
         "</row></Leads>"
   encoded_url = URI.encode(uri)
   url = URI.parse(encoded_url);
