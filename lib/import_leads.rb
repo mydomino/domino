a8403ebd9b9b17ce2c2d18c8c867ac614b748beb
marciandmnd <marciandmnd@yahoo.com>
2016-07-08 12:19:40 -0700

    updated import script for joshfox leads 07072016

--- lib/import_leads.rb
+++ lib/import_leads.rb
@@ -1,22 +1,31 @@
 require 'csv'
+#headers for JoshFox import 070702016:
+#First Name,
+#Last Name,
+# Email Address,
+# Phone Number,
+# Concierge Lead Source,
+# Concierge Lead Status,
+# City,
+# State,
+# Concierge Lead Owner
 
-# Profile.skip_callback(:create, :after, :save_to_zoho)
-path = "#{File.expand_path(File.dirname(__FILE__))}/JoshFox_ZohoImport_06232016.csv"
-# puts path
+Profile.skip_callback(:create, :after, :save_to_zoho)
+path = "#{File.expand_path(File.dirname(__FILE__))}/JoshFox_ZohoImport_07072016.csv"
+puts path
 leads = CSV.read(path, headers:true)
 leads.each do |row|
-#   lead = Profile.create(
-#                             first_name: row["First Name"],
-#                             last_name: row["Last Name"],
-#                             email: row["Email"],
-#                             address_line_1: row["Street"],
-#                             city: row["City"],
-#                             state: row["State"],
-#                             zip_code: row["Zip Code"],
-#                             phone: row["Phone"],
-#                             partner_code_id: PartnerCode.find_by_partner_name("Josh Fox").id,
-#                             onboard_complete: true
-#                           )
+  lead = Profile.create(
+                            first_name: row["First Name"],
+                            last_name: row["Last Name"],
+                            email: row["Email Address"],
+                            phone: row["Phone Number"],
+                            city: row["City"],
+                            state: row["State"],
+                            partner_code_id: PartnerCode.find_by_partner_name("Josh Fox").id,
+                            onboard_complete: true
+                          )
+
   case row["Concierge Lead Owner"]
   when "Laura Osburn"
     @concierge = 'laura@mydomino.com'
@@ -36,25 +45,23 @@ leads.each do |row|
         "<FL val='Lead Owner'>#{@concierge}</FL>"\
         "<FL val='First Name'>#{row['First Name']}</FL>"\
         "<FL val='Last Name'>#{row['Last Name']}</FL>"\
-        "<FL val='Email'>#{row['Email']}</FL>"\
-        "<FL val='Phone'>#{row['Phone']}</FL>"\
-        "<FL val='Lead Source'>Josh Fox</FL>"\
+        "<FL val='Email'>#{row['Email Address']}</FL>"\
+        "<FL val='Phone'>#{row['Phone Number']}</FL>"\
+        "<FL val='Lead Source'>JoshFox</FL>"\
         "<FL val='Street'>#{row['Street']}</FL>"\
         "<FL val='City'>#{row['City']}</FL>"\
         "<FL val='State'>#{row['State']}</FL>"\
-        "<FL val='Zip Code'>#{row['Zip Code']}</FL>"\
         "<FL val='Partner Code'>JOSHFOX</FL>"\
         "<FL val='Partner Code Name'>Josh Fox</FL>"\
         "<FL val='Dashboard Been Registered?'>No</FL>"\
-        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{row['Email']}</FL>"\
+        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{row['Email Address']}</FL>"\
         "<FL val='Onboard Complete'>Yes</FL>"\
-        "<FL val='Description'>Auto Onboard June 24, 2016</FL>"\
+        "<FL val='Description'>Auto Onboard July 7, 2016</FL>"\
         "</row></Leads>"
-  puts uri
   encoded_url = URI.encode(uri)
   url = URI.parse(encoded_url);
   Net::HTTP.post_form(url, {})
-  sleep 1
+  sleep (1.0/2.0)
 end
 
-# Profile.set_callback(:create, :after, :save_to_zoho)
+Profile.set_callback(:create, :after, :save_to_zoho)
