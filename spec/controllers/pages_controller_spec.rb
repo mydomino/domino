2aa57530d1f309d60bc67c25644c59b98001901c
marciandmnd <marciandmnd@yahoo.com>
2016-07-21 17:52:18 -0700

    updated pages controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,13 +2,29 @@ require 'rails_helper'
 require 'pages_controller'
 
 describe PagesController do
-  it 'displays the home page' do
-    get :index
-    expect(response).to render_template(:index)
+  describe "GET index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns @profile" do
+      profile = Profile.new
+      get :index
+      expect(assigns(:profile)).to be_kind_of(Profile)
+    end
   end
 
-  it 'displays the terms page' do
-    get :terms
-    expect(response).to render_template(:terms)
+  describe "GET terms" do
+    it "renders the terms template" do
+      get :terms
+      expect(response).to render_template(:terms)
+    end
   end
 end
+
+  # it 'displays the terms page' do
+  #   get :terms
+  #   expect(response).to render_template(:terms)
+  # end
+# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,27 +1,25 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/.bundle/'
-end
+# require 'simplecov'
+# SimpleCov.start do
+#   add_filter '/.bundle/'
+# end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
+# ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'helpers/zoho_mock'
-require 'helpers/amazon_mock'
-require 'faker'
-include Warden::Test::Helpers
-RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
-end
+# require 'helpers/zoho_mock'
+# require 'helpers/amazon_mock'
+# require 'faker'
+# include Warden::Test::Helpers
+# RSpec.configure do |config|
+#   config.include Devise::TestHelpers, type: :controller
+#   config.include Devise::TestHelpers, type: :view
+# end
 
-
-
-Warden.test_mode!
-WebMock.disable_net_connect!(:allow_localhost => true)
+# Warden.test_mode!
+# WebMock.disable_net_connect!(:allow_localhost => true)
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -42,38 +40,38 @@ WebMock.disable_net_connect!(:allow_localhost => true)
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   
-  config.use_transactional_fixtures = false
+#   config.use_transactional_fixtures = false
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+#   config.before(:suite) do
+#     DatabaseCleaner.clean_with(:truncation)
+#   end
 
-  config.before(:each) do |example|
-    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
-    DatabaseCleaner.start
-  end
+#   config.before(:each) do |example|
+#     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+#     DatabaseCleaner.start
+#   end
   
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+#   config.after(:each) do
+#     DatabaseCleaner.clean
+#   end
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+#   # RSpec Rails can automatically mix in different behaviours to your tests
+#   # based on their file location, for example enabling you to call `get` and
+#   # `post` in specs under `spec/controllers`.
+#   #
+#   # You can disable this behaviour by removing the line below, and instead
+#   # explicitly tag your specs with their type, e.g.:
+#   #
+#   #     RSpec.describe UsersController, :type => :controller do
+#   #       # ...
+#   #     end
+#   #
+#   # The different available types are documented in the features, such as in
+#   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
 
