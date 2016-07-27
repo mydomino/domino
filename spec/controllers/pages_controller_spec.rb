9f0ef83722bc6bc2a5029b2ce54d1aa8dcf15111
marciandmnd <marciandmnd@yahoo.com>
2016-07-26 17:13:54 -0700

    added rspec specs for templating static pages about, terms, privacy, faq, and team

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,16 +15,39 @@ describe PagesController do
     end
   end
 
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template(:about)
+    end
+  end
+  
   describe "GET terms" do
     it "renders the terms template" do
       get :terms
       expect(response).to render_template(:terms)
     end
   end
-end
 
-  # it 'displays the terms page' do
-  #   get :terms
-  #   expect(response).to render_template(:terms)
-  # end
-# end
+  describe "GET team" do
+    it "renders the team template" do
+      get :team
+      expect(response).to render_template(:team)
+    end
+  end
+
+  describe "GET privacy" do
+    it "renders the privacy template" do
+      get :privacy
+      expect(response).to render_template(:privacy)
+    end
+  end
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template(:faq)
+    end
+  end
+
+end
