913598e14f80409f953d4725ecec7266942edac2
Marcian Diamond <marciandmnd@yahoo.com>
2016-09-07 12:53:10 -0700

    Removed analytics filters all/mine as concierge_ids are no longer being assigned to dashboards

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,17 +4,10 @@ class AnalyticsController < ApplicationController
 
   def show
     authorize :analytic, :show?
-    @filter = params[:filter]
-    if(@filter == 'all')
-      @recent_events = Recommendation.done.includes(:recommendable, :dashboard).timestamped.order(:updated_at).last(50)
-    else
-      @filter = 'mine'
-      @recent_events = Recommendation.done.includes(:recommendable, :dashboard).timestamped.where(concierge_id: current_user.id).order(:updated_at).last(50)
-    end
+    @recent_events = Recommendation.done.includes(:recommendable, :dashboard).timestamped.order(:updated_at).last(50)
     @dashboard_count = Dashboard.all.count
     @recommendations_count = Recommendation.all.count
     @recommendations_completed_count = Recommendation.done.count
-    @recommendations = Recommendation.all
     @dashboards_by_state = Dashboard.joins('join leads on leads.email = dashboards.lead_email').where('leads.state IS NOT NULL').group("leads.state").order("COUNT(leads.state) DESC").count
   end
 end
