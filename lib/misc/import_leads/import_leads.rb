cd63a3979eb05ca246829d3d75ad9d87118b12af
Marcian Diamond <marciandmnd@yahoo.com>
2016-11-07 21:08:35 +0100

    Added csv sheet for lead import from fresno fall home show. Massaged import_leads.rb to accomodate this script. Added validation to the script to ensure that first name, last name, and email passes for presence and email passes for valid email address. If any row of csv doesnt pass validation, the script will abort the import operation.

--- lib/misc/import_leads/import_leads.rb
+++ lib/misc/import_leads/import_leads.rb
@@ -1,41 +1,56 @@
 require 'csv'
-#headers for LAGreenFestival import 09192016:
-# "First Name"
-# "Last Name"
-# "Phone"
-# "Email"
-# "Street"
-# "City"
-# "State"
-# "Zip Code"
-# "Avg Electric Bill"
-# "Own or Rent?"
-# "Referred By"
-# "Concierge Lead Source"
-# "Campaign"
-# "Partner Code"
-# "Partner Name"
-# "Concierge Lead Owner"
 
-# Profile.skip_callback(:create, :after, :save_to_zoho)
+def assign_concierge(lead_owner)
+  #assign concierge to lead
+  case lead_owner
+  when "Laura"
+    @concierge = 'laura@mydomino.com'
+  when "Amy"
+    @concierge = 'amy@mydomino.com'
+  else
+    @concierge = 'mel@mydomino.com'
+  end
+end
+
+#headers for Fresno import 11.07.2016:
+
+# Column                    Profile Record        Zoho InsertRecord
+# Concierge Lead Owner      n/a                   yes
+# Concierge Lead Source     n/a                   yes
+# Campaign                  yes                   yes
+# First Name                yes                   yes
+# Last Name                 yes                   yes
+# Phone                     yes                   yes
+# Email                     yes                   yes
+# Street                    yes                   yes
+# City                      yes                   yes
+# State                     yes                   yes
+# Zip Code                  yes                   yes
+# Avg Electric Bill         n/a                   yes
+# Own or Rent?              yes                   yes
+# Referred By               n/a                   yes
+# Partner Code              yes                   yes
+# Partner Name              yes                   yes
+
 Profile.skip_callback(:create, :after, :send_onboard_started_email)
 
 puts "\nFile import name is #{ARGV.first}"
 
 path = "#{File.expand_path(File.dirname(__FILE__))}/import_files/#{ARGV.first}"
-#path = "#{File.expand_path(File.dirname(__FILE__))}/import_files/Bakersfield_ZohoImport_10242016.csv"
 puts "\nFile import path is: #{path}"
 
 # catch CSV exception error
 begin
   leads = CSV.read(path, headers:true)
+  # puts "#{leads.headers}"
 rescue Exception => e  
   puts "\nError! #{e.message}."
   exit
 end
 
-leads.each do |row|
+profiles = []
 
+leads.each_with_index do |row, index|
   lead = Profile.new(
           first_name: row["First Name"],
           last_name: row["Last Name"],
@@ -53,44 +68,60 @@ leads.each do |row|
           onboard_step: 4
         )
 
-  # Dashboard.create(lead_name: "#{lead.first_name} #{lead.last_name}", lead_email: lead.email)
-
-
-  case row["Concierge Lead Owner"]
-  when "Laura"
-    @concierge = 'laura@mydomino.com'
-  when "Amy"
-    @concierge = 'amy@mydomino.com'
+  if lead.valid?
+    #if valid push profile instance into profiles array
+    #meta data is used for info that is not required on Profile record,
+    #but is required in the Zoho Lead Record
+    profiles << { profile: lead, 
+                  meta_data: { 
+                    concierge: assign_concierge(row['Concierge Lead Owner']),
+                    avg_electric_bill: row['Avg Electric Bill'],
+                    referred_by: row['Referred By']
+                  }
+                }
   else
-    @concierge = 'mel@mydomino.com'
+    puts "Aborting import; Profile record invalid at row #{index}"
+    puts "Error: #{lead.errors.full_messages}"
+    exit
   end
+end
 
-  p "processing zoho record for: #{row['Email']}"
+#Profile records have been validated, 
+#now save and create respective dashboards
+#Push to zoho
+puts "Profile records have passed validation."
+puts "Saving Profile records. Creating Dashboards. Pushing Records to zoho...."
+
+profiles.each do |p| 
+  p[:profile].save
+  Dashboard.create(lead_name: "#{p[:profile].first_name} #{p[:profile].last_name}", lead_email: p[:profile].email)
+
+  #Zoho InsertRecord
+  puts "Processing zoho record for: #{p[:profile].email}"
 
   uri = "https://crm.zoho.com/crm/private/xml/Leads/insertRecords?"\
         "newFormat=1"\
         "&authtoken=43a02c5e40acfc842e2e8ed75424ecdf"\
         "&scope=crmapi"\
         "&xmlData=<Leads><row no='1'>"\
-        "<FL val='Lead Owner'>#{@concierge}</FL>"\
-        "<FL val='Lead Source'>#{row['Concierge Lead Source']}</FL>"\
-        "<FL val='First Name'>#{row['First Name']}</FL>"\
-        "<FL val='Last Name'>#{row['Last Name']}</FL>"\
-        "<FL val='Email'><![CDATA[#{CGI.escape(lead.email)}]]></FL>"\
-        "<FL val='Phone'>#{row['Phone']}</FL>"\
-        "<FL val='Lead Source'>#{row['Concierge Lead Source']}</FL>"\
-        "<FL val='Street'><![CDATA[#{CGI.escape(row['Street']) if !row['Street'].nil?}]]></FL>"\
-        "<FL val='City'>#{row['City']}</FL>"\
-        "<FL val='State'>#{row['State']}</FL>"\
-        "<FL val='Zip Code'>#{row['Zip Code']}</FL>"\
-        "<FL val='Own or Rent?'>#{row['Own or Rent?']}</FL>"\
-        "<FL val='Avg Electric Bill'>#{row['Avg Electric Bill']}</FL>"\
-        "<FL val='Referred By'>#{row['Referred By']}</FL>"\
-        "<FL val='Campaign'>#{row['Campaign']}</FL>"\
-        "<FL val='Partner Code'>#{lead.partner_code.code}</FL>"\
-        "<FL val='Partner Code Name'>#{lead.partner_code.partner_name}</FL>"\
+        "<FL val='Lead Owner'>#{p[:meta_data][:concierge]}</FL>"\
+        "<FL val='Lead Source'>Booth Event</FL>"\
+        "<FL val='First Name'>#{p[:profile].first_name}</FL>"\
+        "<FL val='Last Name'>#{p[:profile].last_name}</FL>"\
+        "<FL val='Email'><![CDATA[#{CGI.escape(p[:profile].email)}]]></FL>"\
+        "<FL val='Phone'>#{p[:profile].phone}</FL>"\
+        "<FL val='Street'><![CDATA[#{CGI.escape(p[:profile].address_line_1) if !p[:profile].address_line_1.nil?}]]></FL>"\
+        "<FL val='City'>#{p[:profile].city}</FL>"\
+        "<FL val='State'>#{p[:profile].state}</FL>"\
+        "<FL val='Zip Code'>#{p[:profile].zip_code}</FL>"\
+        "<FL val='Own or Rent?'>#{p[:profile].housing}</FL>"\
+        "<FL val='Avg Electric Bill'>#{p[:meta_data][:avg_electric_bill]}</FL>"\
+        "<FL val='Referred By'>#{p[:meta_data][:referred_by]}</FL>"\
+        "<FL val='Campaign'>Fresno Fall Home Improvement</FL>"\
+        "<FL val='Partner Code'>#{p[:profile].partner_code.code}</FL>"\
+        "<FL val='Partner Code Name'>#{p[:profile].partner_code.partner_name}</FL>"\
         "<FL val='Dashboard Been Registered?'>No</FL>"\
-        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{CGI.escape(CGI.escape(lead.email))}</FL>"\
+        "<FL val='Dashboard Registration URL'>mydomino.com/users/sign_up?email=#{CGI.escape(CGI.escape(p[:profile].email))}</FL>"\
         "<FL val='Onboard Complete'>Yes</FL>"\
         "<FL val='Description'>#{Time.new.strftime('Auto onboard %m/%d/%Y')}</FL>"\
         "</row></Leads>"
@@ -101,3 +132,4 @@ leads.each do |row|
 end
 
 Profile.set_callback(:create, :after, :send_onboard_started_email)
+
