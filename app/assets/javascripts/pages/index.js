fb4abf02d3d0c3dd57e677186de93c31ddfce7c5
Marcian Diamond <marciandmnd@yahoo.com>
2017-01-16 19:49:04 -0800

    Load homepage javascript with modulejs. Minor refactor of homepage javascipt

--- app/assets/javascripts/pages/index.js
+++ app/assets/javascripts/pages/index.js
@@ -1,58 +1,82 @@
-window.MyDomino = window.MyDomino || {};
-window.MyDomino.pages = window.MyDomino.pages || {};
+modulejs.define('home', function () {
+  return function(){
+    // Begin module scope variable definition and initialization
+    var $body,
+        $playCircle,
+        $videoModal,
+        $closeModal,
+        $txtInputFirstName,
+        $btnBecomeAMember,
+        iframe,
+        player,
+        onFinish;
 
-window.MyDomino.pages["pages-index"] = function(){
-  var iframe = $('iframe')[0];
-  var player = $f(iframe);
-  // When the player is ready/loaded, add a finish event listener
-  player.addEvent('ready', function() {
-    player.addEvent('finish', onFinish);
-  });
+    $body = $('body');
+    $playCircle = $('.fa-play-circle');
+    $videoModal = $('#video-modal');
+    $closeModal = $('#close-modal');
+    $txtInputFirstName = $('#profile_first_name');
+    $btnBecomeAMember = $('#btn-become-a-member');
+    iframe = $('iframe')[0];
+    player = $f(iframe);
 
-  function onFinish(id) {
-    $('#video-modal').fadeOut();
-    player.api('unload');
-    $('body').removeClass('lock-position');
-  }
-  $('.fa-play-circle').on('click', function(){
-    $('#video-modal').fadeIn();
-    $('body').addClass('lock-position');
-    player.api('play');
-  });
+    // When the player is ready/loaded, add a finish event listener
+    player.addEvent('ready', function() {
+      player.addEvent('finish', onFinish);
+    });
 
-  $('#close-modal').on('click', function(){
-    $('#video-modal').fadeOut();
-    player.api('unload');
-    $('body').removeClass('lock-position');
-  });
+    // Begin /onFinish
+    onFinish = function(id) {
+      $videoModal.fadeOut();
+      player.api('unload');
+      $body.removeClass('lock-position');
+    }
+    // End /onFinish
 
-  $('#btn-become-a-member').on('click', function(){
-    $('html, body').animate({
-        scrollTop: 0
-      },
-      1000,
-      function(){ $('#profile_first_name').focus(); }
-    );
-  });
+    // Begin module scope event handlers
+    $playCircle.on('click', function(){
+      $videoModal.fadeIn();
+      $body.addClass('lock-position');
+      player.api('play');
+    });
 
-  // Name and email form validation
-  nameAndEmailFormValidate();
+    $closeModal.on('click', function(){
+      $videoModal.fadeOut();
+      player.api('unload');
+      $body.removeClass('lock-position');
+    });
 
-  $('.slick').slick({
-    centerMode: true,
-    centerPadding: '20%',
-    slidesToShow: 1,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 640,
-        settings: {
-          centerPadding: '8%'
+    $btnBecomeAMember.on('click', function(){
+      $('html, body').animate({
+          scrollTop: 0
+        },
+        1000,
+        function(){ $txtInputFirstName.focus(); }
+      );
+    });
+    // End module scope event handlers
+
+    // Name and email form validation
+    nameAndEmailFormValidate();
+
+    // Slick carousel
+    $('.slick').slick({
+      centerMode: true,
+      centerPadding: '20%',
+      slidesToShow: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 640,
+          settings: {
+            centerPadding: '8%'
+          }
         }
-      }
-    ]
-  });
-};
+      ]
+    });
+    // End Slick carousel
+  };
+});
