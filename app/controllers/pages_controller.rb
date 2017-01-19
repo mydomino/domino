52d5f48eecc125d44af37cce3a6ae24d012628a3
Marcian Diamond <marciandmnd@yahoo.com>
2017-01-19 15:41:51 -0800

    Removed unneeded comments from PagesController#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,6 @@ class PagesController < ApplicationController
   layout 'example', only: :example
 
   def index
-    #continue onboarding
-    # if params.has_key?(:profile_id) && @profile = Profile.find(params[:profile_id])
-    #   if @profile.onboard_step == 0
-    #     Profile.skip_callback(:update, :after, :update_zoho)
-    #     @profile.update(onboard_step: 1)
-    #     Profile.set_callback(:update, :after, :update_zoho)
-    #   end
-    #   interest_form_resources if (@profile.onboard_step == 1)
-    #   get_partner_code if (@profile.onboard_step == 3)
-    #   flash.now[:notice] = "Welcome back, #{@profile.first_name.capitalize}! Here is where you left off."
-    #   @response = {form: "profiles/#{FORMS[@profile.onboard_step]}", method: :put}
-    #   return
-    # end
-
     #user goes back from wizard form to homepage
     if params.has_key?(:profile_id) && @profile = Profile.find(params[:profile_id])
       @response = {form: 'profiles/name_and_email', method: :put}
