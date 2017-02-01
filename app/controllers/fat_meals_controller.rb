f08e57f645632125973ac4d963f767d02e1313af
Marcian Diamond <marciandmnd@yahoo.com>
2017-02-01 15:13:17 -0800

    Retrieve and save FAT records with user timezone

--- app/controllers/fat_meals_controller.rb
+++ app/controllers/fat_meals_controller.rb
@@ -5,37 +5,39 @@ class FatMealsController < ApplicationController
   # GET /food-action-tracker/:year/:month/:day
   # Purpose: Display the FAT interface for a given date
   #  If no date params are provided, the interface will
-  #  render for the current date
+  #  render for the current date based on users timezone
   def edit
-    #TODO get date by user timezone
+    time_zone_name = Time.zone.name
+
     if params[:year].present?
       year = params[:year].to_i
       month = params[:month].to_i
       day = params[:day].to_i
 
-      fat_date = Date.new(year, month, day)
+      time = Time.new(year, month, day).in_time_zone(time_zone_name)
     else
-      time_zone_name = Time.zone.name
+      # Set FAT date based on user timezone
+      # This is made possible by the browser-timezone-rails gem
       time = Time.now.in_time_zone(time_zone_name)
-      fat_date = Date.new(time.year, time.month, time.day)
     end
 
-    @prev_date = fat_date - 1.day
-    @next_date = fat_date + 1.day
+    @prev_date = time - 1.day
+    @next_date = time + 1.day
     @current_date = Date.today
     
-    meal_day = MealDay.includes(meals: [:meal_type, :foods]).find_by(user: current_user, date: fat_date)
+    meal_day = MealDay.includes(meals: [:meal_type, :foods]).find_by(user: current_user, date: time)
 
     @fat_day = {
       meal_day: meal_day,
       meals: meal_day ? meal_day.meals.order(:meal_type_id).as_json(:include => [:meal_type, :foods]) : new_meals,
-      date: fat_date,
+      date: time,
       meal_type: MealType.all,
       food_types: FoodType.all
     }
   end
 
   # POST /food-action-tracker
+  # Create FAT resources for the date provided in params.
   def create
     date_param = params[:fat_day][:date]
     meals = params[:fat_day][:meals]
@@ -62,10 +64,10 @@ class FatMealsController < ApplicationController
       end
     end
 
-    # carbon_footprint = calculate_carbon_footprint
-    # meal_day.update(carbon_footprint: carbon_footprint)
+    meal_day.update(carbon_footprint: calculate_carbon_footprint)
+
     render json: {
-      carbon_footprint: 100,
+      carbon_footprint: meal_day.carbon_footprint,
       meals: meal_day.meals.order(:meal_type_id).as_json(:include => [:meal_type, :foods]),
       meal_day: meal_day,
       status: 200
@@ -73,6 +75,7 @@ class FatMealsController < ApplicationController
   end
 
   # PATCH /food-action-tracker/
+  # Update FAT resources for date provided in params
   def update
     meal_day_id = params[:meal_day][:id].to_i
     meal_day = MealDay.find(meal_day_id)
@@ -110,6 +113,8 @@ class FatMealsController < ApplicationController
     100
   end
 
+  # /new_meals/
+  # Purpose: For new FAT views, pass default meal objects to React
   def new_meals
     [
       {
