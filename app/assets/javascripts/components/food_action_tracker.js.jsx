dac74a695f2d18768eddce8fc727a306e08f93df
Marcian Diamond <marciandmnd@yahoo.com>
2017-02-07 17:13:28 -0800

    Vertical slider for food size.  Add active state to food items.

--- app/assets/javascripts/components/food_action_tracker.js.jsx
+++ app/assets/javascripts/components/food_action_tracker.js.jsx
@@ -29,7 +29,7 @@ class FoodActionTracker extends React.Component {
   render() {
     var that = this;
     var foodTypes = this.props.fatDay.food_types.map(function(foodType, index){
-                      return <FoodType food={that.state.foods[foodType.id]} index={index} key={foodType.name} foodType={foodType} updateFoodSize={(f)=>that.updateFoodSize(f)} />
+                      return <FoodType sizeInfo={that.props.foodSizeInfo[foodType.id]} food={that.state.foods[foodType.id]} index={index} key={foodType.name} foodType={foodType} updateFoodSize={(f)=>that.updateFoodSize(f)} />
                     });
     return (
       <div className='remodal-bg'>
@@ -63,3 +63,49 @@ class FoodActionTracker extends React.Component {
     alert('cf');
   }
 }
+FoodActionTracker.defaultProps = {
+  foodSizeInfo : {
+    "1" : {
+        "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+        "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+        "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+        "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+        "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    },
+    "2" : {
+      "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+      "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+      "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+      "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+      "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    },
+    "3" : {
+      "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+      "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+      "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+      "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+      "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    },
+    "4" : {
+      "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+      "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+      "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+      "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+      "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    },
+    "5" : {
+      "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+      "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+      "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+      "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+      "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    },
+    "6" : {
+      "0" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor", 
+      "50" : "Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", 
+      "100" : "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.", 
+      "150" : "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.",
+      "200" : "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus."
+    }
+  }
+};
--- app/assets/javascripts/components/food_type.js.jsx
+++ app/assets/javascripts/components/food_type.js.jsx
@@ -1,26 +1,33 @@
+
+
 class FoodType extends React.Component {
   componentWillMount() {
-    let food = Object.assign({food_type_id: this.props.foodType.id}, this.props.food);
+    let active = this.props.food != null ? true : false;
+
+    let food = Object.assign({food_type_id: this.props.foodType.id, size: 100}, this.props.food);
     this.setState({
-      food: food
+      food: food,
+      active: active
     });
   }
   updateFoodSize(newSize){
     let food = Object.assign({}, this.state.food);
     food.size = newSize;
+    let active = newSize != 0 ? true : false;
 
     this.setState({
-      food: food 
+      food: food, 
+      active: active
     });
 
     this.props.updateFoodSize(this.state.food);
   }
   render() {
-    let size = this.state.food ? this.state.food.size : 0;
-
+    // let size = this.state.food ? this.state.food.size : 100;
+    //this.props.bgColorMap[this.state.food.size]
     return (
       <div>
-        <div  onClick={()=>this.remodal()} style={{backgroundColor: this.props.bgColorMap[size]}} className="col col-6 sm-col sm-col-4 relative food-type" >
+        <div  onClick={()=>this.remodal()} style={{backgroundColor: (this.state.active) ? this.props.bgColorMap[this.state.food.size] : 'white' }} className="col col-6 sm-col sm-col-4 relative food-type" >
           <div className='flex flex-column items-center border border-gray-30 p2 pointer inline-block' >
             <div className="p2 inline-block" style={{width: '64px', height: '64px'}}>
               <img src={"/fat_icons/" + this.props.foodType.icon} />
@@ -32,36 +39,48 @@ class FoodType extends React.Component {
         </div>
 
         <div data-remodal-id={this.props.index + "-modal"}>
-          <button data-remodal-action="close" className="remodal-close"></button>
-            <div className="flex items-center justify-center">
-              <img src={"/fat_icons/" + this.props.foodType.icon} />
-              <div className="h1 ml1">{this.props.foodType.name}</div>
+            <button data-remodal-action="close" className="remodal-close"></button>
+              <div className="flex items-center justify-center">
+                <img src={"/fat_icons/" + this.props.foodType.icon} />
+                <div className="h1 ml1">{this.props.foodType.name}</div>
+              </div>
+            <br/>
+            <div className="h2">How much did you eat? </div>
+            <div className="clearfix p2">
+              <div className="col col-12 sm-col sm-col-3">
+                <div className="flex flex-column items-center justify-center">
+                  <div className="meal-size-label mb1 h3">
+                    {this.state.food.size}
+                  </div>
+                  <div id={this.props.index + "-slider"}></div>
+                </div>
+              </div>
+              <div className="size-information col col-12 sm-col sm-col-8 left-align">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus et totam saepe nulla vero fugit labore praesentium error voluptatem corrupti officia, aliquam doloribus maiores autem illo! Voluptates fugiat, quae aut.
+              </div>
             </div>
-          <br/>
-          <div className="h2">How much did you eat? </div>
-          <div id={this.props.index + "-slider"}></div>
-          <div className="m2">
-            <button data-remodal-action="cancel" className="remodal-cancel">Cancel</button>
-            <button data-remodal-action="confirm" className="remodal-confirm">OK</button>
+            <div className="m2 clearfix">
+              <button data-remodal-action="cancel" className="remodal-cancel">Cancel</button>
+              <button data-remodal-action="confirm" className="remodal-confirm">OK</button>
+            </div>
           </div>
-        </div>
-
       </div>
     );
   }
   componentDidMount() {
-    let size = this.state.food ? this.state.food.size : 0;
+    let size = this.state.food.size;
 
     this.$modal =  $('[data-remodal-id=' + this.props.index + '-modal]').remodal();
     $slider = $( "#" + this.props.index + "-slider");
 
     $slider.slider({
+      orientation: "vertical",
       range: "min",
       animate: "fast",
       min:0,
-      max: 3,
-      step: 1,
-      value: size,
+      max: 200,
+      step: 50,
+      value: 100,
       slide: function(event, ui){
         this.updateFoodSize(ui.value);
       }.bind(this)
@@ -72,5 +91,11 @@ class FoodType extends React.Component {
   }
 }
 FoodType.defaultProps = {
-  bgColorMap : ["white", "#87D37C", "#00ccff", "#E26A6A"]
+  bgColorMap : { 
+    "0" : "white", 
+    "50" : "#87D37C", 
+    "100" : "#00ccff", 
+    "150" : "#F1A9A0",
+    "200" : "#E26A6A"
+  }
 };
