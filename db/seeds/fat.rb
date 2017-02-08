c48b6a3f02a9f8f958a9c169b76e4f96c7ea530e
Marcian Diamond <marciandmnd@yahoo.com>
2017-02-08 15:29:39 -0800

    Added carbon footprints to FAT seed file

--- db/seeds/fat.rb
+++ db/seeds/fat.rb
@@ -2,15 +2,28 @@
 # This file may be used to for seeding data required by the food action tracker
 
 # BEGIN FoodType Seeds
+# CF map
+cf_map = {
+  "fruits": 4.6,
+  "vegetables": 2.8,
+  "dairy": 4.5,
+  "grains": 1.3,
+  "fish_poultry_pork": 3.8,
+  "beef_lamb": 14.1
+}.with_indifferent_access
+
+
 FoodType.destroy_all
+
+# Reset PK key to start at 1
 ActiveRecord::Base.connection.reset_pk_sequence!('food_types')
 
-FoodType.categories.each do |category|
+FoodType.categories.each do |key, value|
   FoodType.create(
-    category: category[0],
-    carbon_footprint: 50,
-    icon: "#{category[0]}_icon.png",
-    name: category[0].split('_').join(', ')
+    category: key,
+    carbon_footprint: cf_map[key],
+    icon: "#{key}_icon.png",
+    name: key.split('_').join(', ')
   )
 end
 # END FoodType Seeds
