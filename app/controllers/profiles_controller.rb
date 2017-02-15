4525a2e17baeb1d07b20d085e4bf827443d5ecc1
Marcian Diamond <marciandmnd@yahoo.com>
2017-02-15 11:37:07 -0800

    Display leaderboard list on myhome

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -31,7 +31,7 @@ class ProfilesController < ApplicationController
       fat_graph_date += 1.day
     end
 
-    # @leader_board_list = get_leader_board(@user)
+    @leaderboard_users = cfp_ranking
   end
 
   def verify_current_password
@@ -147,9 +147,32 @@ class ProfilesController < ApplicationController
 
   private
 
-  # def get_leaderboard(user)
-  #   user.organization.get_leaderboard
-  # end
+  # /cfp_ranking/
+  # Purpose: returns list of users to be displayed on the leaderboard
+  def cfp_ranking
+
+    organization = current_user.organization
+
+    if organization.nil?
+      # find the default organization
+      organization = Organization.find_by!(name: 'MyDomino')
+    end
+
+    users = User.where(["organization_id = ?", organization.id])
+
+    #set up date range
+    start_date = Time.zone.today 
+    end_date = Time.zone.today - 60.days
+
+    # refresh the total reward points
+    users.each do |u|
+
+      # calculate user reward points during the period and save it to the user's member variable
+      u.get_fat_reward_points(start_date, end_date)
+    end
+
+    @users = User.includes(:profile).where(["organization_id = ?", organization.id]).order("fat_reward_points DESC").first(8)
+  end
 
   def create_dashboard(profile)
     Dashboard.create(lead_name: "#{profile.first_name} #{profile.last_name}", lead_email: profile.email)
