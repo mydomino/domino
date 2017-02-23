7282dd15be9d1c94a9c0e02bd416320c727bc96f
John Pang <johnp@mydomino.com>
2017-02-23 15:37:28 -0800

    HOTFIX: Fixed carbon emission formula
    We were treating the food_type.carbon_footprint value as kilograms and
    just multiplying it by the food size. That value is actually in grams
    and so we need to multiple by the average calorie size of the food type.
    
    Also added seeds for average size of each food type.

--- app/models/meal_day.rb
+++ app/models/meal_day.rb
@@ -16,16 +16,19 @@
 #  fk_rails_bef09397d1  (user_id => users.id)
 #
 
-
-
-
 class MealDay < ActiveRecord::Base
   belongs_to :user
   has_many :foods, dependent: :destroy
   validates :user, :date, presence: :true
 
+  # Carbon footprint calculation formula:
+  #   Average size (in Calories) of food type x Size (percentage) x Carbon footprint of that food type (g/Calories)
   def calculate_cf
-    self.carbon_footprint = (self.foods.inject(0) {|sum, f| sum + (f.food_type.carbon_footprint * (f.size/100.0))}).round(2)
+    self.carbon_footprint = self.foods.inject(0) do |sum, f| 
+      sum + (f.food_type.carbon_footprint * (f.size / 100.0) * f.food_type.average_size)
+    end
+    self.carbon_footprint += 1064 # add baseline carbon emission (1064g) for oils, snacks & sugar, and drinks
+    self.carbon_footprint = (self.carbon_footprint / 1000).round(2) # Convert from g to kg
     self.save
   end
 
--- db/seeds/fat.rb
+++ db/seeds/fat.rb
@@ -2,8 +2,9 @@
 # This file may be used to for seeding data required by the food action tracker
 
 # BEGIN FoodType Seeds
+
 # CF map
-# C02 emission values in grams per KiloCalorie
+# C02 emission values in grams per Calorie
 cf_map = {
   "fruits": 4.6,
   "vegetables": 2.8,
@@ -13,6 +14,16 @@ cf_map = {
   "beef_lamb": 14.1
 }.with_indifferent_access
 
+# Avg food size map
+# Average daily food size (in Calories)
+avg_food_size_map = {
+  "fruits": 95,
+  "vegetables": 112,
+  "dairy": 278,
+  "grains": 618,
+  "fish_poultry_pork": 238,
+  "beef_lamb": 156
+}.with_indifferent_access
 
 FoodType.destroy_all
 
@@ -24,7 +35,8 @@ FoodType.categories.each do |key, value|
     category: key,
     carbon_footprint: cf_map[key],
     icon: "#{key}_icon.png",
-    name: key.split('_').map{|e| e.capitalize}.join(', ')
+    name: key.split('_').map{|e| e.capitalize}.join(', '),
+    average_size: avg_food_size_map[key]
   )
 end
 # END FoodType Seeds
