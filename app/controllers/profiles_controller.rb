5f2e32e495781dd960bcd69dfe07384f2504b3c3
Marcian Diamond <marciandmnd@yahoo.com>
2017-03-06 14:23:07 -0800

    Remove fat graph code from ProfilesController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,38 +17,9 @@ class ProfilesController < ApplicationController
   def myhome
     # @user used to display membership type, member since, and renewal date info
     @user = current_user
-
-    # # @profile used to display first and last name
+    # @profile used to display first and last name
     @profile = @user.profile
-
-    # @fat_graph_cf_map = {}
-    # time_zone_name = Time.zone.name
-    # time_now = Time.now.in_time_zone(time_zone_name)
-    # today = Date.new(time_now.year, time_now.month, time_now.day)
-
-    # active_days = today.cwday
-    # days_left = 7 - today.cwday
-    # fat_graph_date = today - active_days.days + 1
-    # @cf = []
-
-
-    # active_days.times do
-    #   meal_day = MealDay.find_by(date: fat_graph_date, user: current_user)
-    #   cf = meal_day ? meal_day.carbon_footprint : nil
-    #   points = meal_day ? (meal_day.points || 0) : 0
-    #   @cf << {cf: cf, pts: points || 0, path: fat_graph_date.to_s.split("-").join("/")}
-    #   # @fat_graph_cf_map[fat_graph_date.to_s] = meal_day ? meal_day.carbon_footprint : 6.2
-    #   fat_graph_date += 1.day
-    # end
     @leaderboard_users = cfp_ranking
-    # days_left.times do 
-    #   @cf << {cf: "future", pts: "future"}
-    # end
-    # # @cf = []
-
-    # 7.times do 
-    #   @cf << {cf: nil, pts: nil}
-    # end
   end
 
   def verify_current_password
