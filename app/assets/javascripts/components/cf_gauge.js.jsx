33dbe249b873fdd681e643affe93830bcaac26d4
Jimmy <jyng02@gmail.com>
2017-03-09 14:52:32 -0800

    WIP of gauge color to match mockups

--- app/assets/javascripts/components/cf_gauge.js.jsx
+++ app/assets/javascripts/components/cf_gauge.js.jsx
@@ -25,7 +25,7 @@ class CfGauge extends React.Component {
   render(){
     return(
       <div>
-        <canvas id="gauge"  
+        <canvas id="gauge"
           style={{ opacity: (this.props.cf == null) ? 0.4 : 1 }}></canvas>
 
         <div className="bg-white h2 medium white my0 line-height-3 black">{ (this.props.method == "POST") ? "-" : this.state.cf } kg <div className="h3 mt0">Carbon Emissions</div>
@@ -40,13 +40,13 @@ class CfGauge extends React.Component {
       radiusScale: 1, // Relative radius
       pointer: {
         length: 0.6, // // Relative to gauge radius
-        strokeWidth: 0.035, // The thickness
-        color: '#000000' // Fill color
+        strokeWidth: 0.1, // The thickness
+        color: '#00CCA0' // Fill color
       },
       limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
       limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
-      colorStart: '#6FADCF',   // Colors
-      colorStop: '#8FC0DA',    // just experiment with them
+      colorStart: '#A6FFD6',   // Colors
+      colorStop: '#FF7A7A',    // just experiment with them
       strokeColor: '#E0E0E0',  // to see which ones work best for you
       generateGradient: true,
       highDpiSupport: true,    // High resolution support
@@ -58,9 +58,8 @@ class CfGauge extends React.Component {
         fractionDigits: 1  // Optional: Numerical precision. 0=round off.
       },
       staticZones: [
-         {strokeStyle: "#87D37C", min: 0, max: 4.1},
-         {strokeStyle: "#81CFE0", min: 4.1, max: 8.2},
-         {strokeStyle: "#E26A6A", min: 8.2, max: 12.4}
+         {strokeStyle: "#00FFC4", min: 0, max:8.2},
+         {strokeStyle: "#FF7A7A", min: 8.2, max: 12.4}
       ]
     };
     var target = document.getElementById('gauge'); // your canvas element
