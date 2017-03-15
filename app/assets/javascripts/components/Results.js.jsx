dd6af50d589d4d6235ea005df5f885f2d81e598b
Jimmy <jyng02@gmail.com>
2017-03-14 18:11:36 -0700

    adding blog article header pictures. updated both myhome and learn more

--- app/assets/javascripts/components/Results.js.jsx
+++ app/assets/javascripts/components/Results.js.jsx
@@ -8,23 +8,23 @@ class Results extends React.Component {
         <div className="h4 caps bold gray-80 mt3 mb2">WEEKLY PROGRESS
         </div>
         <FatGraph ref="fatgraph" graph_params={this.props.graph_params}/>
-        <div className="pt3 pb4 center" style={{backgroundColor: "#C5EFF7"}}>
+        <div className="pt3 pb4 center bg-gray-05">
           <div className="mx-auto">
             <div className="h4 caps bold gray-80 my3">LEARN MORE
             </div>
             <div className="max-width-4 mx-auto">
               <div className="flex flex-column sm-row justify-around">
-                <div className="bg-white m1">
-                  <img src="http://placehold.it/200x150"/>
-                    <h4 className="p1">Appetizers: Your first course</h4>
+                <div className="bg-white m2 rounded-bottom">
+                  <img src="/fat_icons/appetizers.jpg" className="fill-x rounded-top"/>
+                    <h3 className="p1 left-align">Appetizers: Your first course</h3>
                 </div>
-                <div className="bg-white m1">
-                  <img src="http://placehold.it/200x150"/>
-                    <h4 className="p1">Appetizers: Your first course</h4>
+                <div className="bg-white m2 rounded-bottom">
+                  <img src="/fat_icons/cow-tongue.jpg" className="fill-x rounded-top"/>
+                    <h3 className="p1 left-align">Are cows the key to saving the planet?</h3>
                 </div>
-                <div className="bg-white m1">
-                  <img src="http://placehold.it/200x150"/>
-                    <h4 className="p1">Appetizers: Your first course</h4>
+                <div className="bg-white m2 rounded-bottom">
+                  <img src="/fat_icons/ugly-apples.jpg" className="fill-x rounded-top"/>
+                    <h3 className="p1 left-align">Hidden health. Benefits of ugly produce</h3>
                   </div>
               </div>
               </div>
