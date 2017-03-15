0212cab6fadd2ce275cf3df918be0f04360b13dd
Jimmy <jyng02@gmail.com>
2017-03-15 12:00:28 -0700

    adding links and description WIP

--- app/assets/javascripts/components/Results.js.jsx
+++ app/assets/javascripts/components/Results.js.jsx
@@ -14,18 +14,30 @@ class Results extends React.Component {
             </div>
             <div className="max-width-4 mx-auto">
               <div className="flex flex-column sm-row justify-around">
+
                 <div className="bg-white m2 rounded-bottom">
+                <a href="https://actions.mydomino.com/" className="text-decoration-none black">
                   <img src="/fat_icons/appetizers.jpg" className="fill-x rounded-top"/>
                     <h3 className="p1 left-align">Appetizers: Your first course</h3>
+                  </a>
                 </div>
+
+
                 <div className="bg-white m2 rounded-bottom">
+                  <a href="https://mydomino.com/articles/cows-climate-change" className="text-decoration-none black">
                   <img src="/fat_icons/cow-tongue.jpg" className="fill-x rounded-top"/>
-                    <h3 className="p1 left-align">Are cows the key to saving the planet?</h3>
+                    <h3 className="px1 left-align mb0">Are cows the key to saving the planet?</h3>
+                    <p className="h5 left-align gray-80 px1 mt1 mb3 col-10">They may look sweet and innocent, but don’t let that fool you!
+                    </p>
+                    </a>
                 </div>
+
+                <a href="https://mydomino.com/articles/hidden-health-benefits-of-ugly-produce/">
                 <div className="bg-white m2 rounded-bottom">
                   <img src="/fat_icons/ugly-apples.jpg" className="fill-x rounded-top"/>
                     <h3 className="p1 left-align">Hidden health. Benefits of ugly produce</h3>
                   </div>
+                  </a>
               </div>
               </div>
             </div>
