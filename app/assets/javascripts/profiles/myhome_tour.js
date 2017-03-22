b402ecd3b4f8e7da8b1484ac2099118ca0149ef2
Marcian Diamond <marciandmnd@yahoo.com>
2017-03-22 10:25:36 -0700

    Extracted myhome membership benefits cards into partials.  Added introjs code to give a tour of the myhome elements

--- app/assets/javascripts/profiles/myhome_tour.js
+++ app/assets/javascripts/profiles/myhome_tour.js
@@ -1,5 +1,31 @@
 modulejs.define('myhome_tour', function(){
   return function() {
+    var firstModal = $('.remodal#first').remodal();
+    var secondModal = $('.remodal#second').remodal();
+    var thirdModal = $('.remodal#third').remodal();
     
+    firstModal.open();
+
+    $('.remodal-overlay').remove();
+
+    $('#first .remodal-confirm').on('click', function(){
+      $(this).parent().fadeOut(function() {
+        secondModal.open();
+      });
+    });
+
+    $('#second .remodal-confirm').on('click', function(){
+      $(this).parent().fadeOut(function() {
+        thirdModal.open();
+      });
+    });
+
+    $('#third .remodal-confirm').on('click', function(){
+      thirdModal.close();
+      thirdModal.destroy();
+      $('.welcome-tour-bg').fadeOut(function() {
+        introJs().start();
+      });
+    });
   };
 });
