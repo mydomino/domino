0ed43b1665508c8009fb6e4fff1def8e68cdc5b0
Marcian Diamond <marciandmnd@yahoo.com>
2017-05-09 12:47:39 -0700

    Removed track_event calls

--- app/controllers/fat_meals_controller.rb
+++ app/controllers/fat_meals_controller.rb
@@ -49,10 +49,6 @@ class FatMealsController < ApplicationController
       food_type = food.food_type.category
       size = food.size
     end 
-    track_event "Initiate Food tracking", { "User": current_user.email,
-      "food_date": @meal_day.date,
-      "food_type": food_type,
-      "food_size": size }
 
     fat_graph_params(@date)
     render_response
@@ -84,11 +80,7 @@ class FatMealsController < ApplicationController
       food_type = food.food_type.category
       size = food.size
     end 
-    track_event "Update Food", { "User": current_user.email,
-      "food_date": @meal_day.date,
-      "food_type": food_type,
-      "food_size": size }
-
+    
     fat_graph_params(@meal_day.date)
 
     render_response
