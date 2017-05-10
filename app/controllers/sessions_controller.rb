2a39b5c3c619fb97e4a44e57447ef60b8e956703
John Pang <johnp@mydomino.com>
2017-05-09 18:31:30 -0700

    Change login event name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,6 @@ class SessionsController < Devise::SessionsController
 
   def create
     super
-    @tracker.track(current_user.id, "User session created");
+    @tracker.track(current_user.id, "User logged in");
   end
 end
